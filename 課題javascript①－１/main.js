4b1020ed3af354bb5dc93458e01b3bbbbe90ea2d
shinnosuke565 <damking420@gmail.com>
2022-04-18 15:24:53 +0900

    first commit

---
+++ 課題javascript①－１/main.js
@@ -0,0 +1,21 @@
+//課題①-1
+// for (var i = 3; i <= 100 ; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log("FizzBuzz")
+//   } else if (i % 3 == 0) {
+//     console.log("Fizz")
+//   } else if (i % 5 == 0) {
+//     console.log("Buzz")
+//   } else {
+//     console.log(i  )
+//   }
+// }
+
+//課題①-2
+let shutoken = prompt("日本の首都は？");
+if (shutoken == "東京") {
+  alert("正解です！");
+} else {
+  alert("不正解です！");
+}
+
