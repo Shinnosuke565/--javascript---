ac527243352bdcd8d2cc243f2f20a940116f5f44
shinnosuke565 <damking420@gmail.com>
2023-07-02 11:27:13 +0900

    学習しました

--- 課題javascript①－１/main.js
+++ 課題javascript①－１/main.js
@@ -22,47 +22,36 @@
 //----------------------------------------------
 
 //課題①-3
-
-//定数を定義する
 const text1 = ('===============================');
 const text2 = ('現在持っているタスクの一覧');
 
-//関数を定義する
 function add() {
   console.log(text1);
   console.log(text2);
   console.log(text1);
 }
 
-//コンソールに出力
 add();
 
-//タスク一覧
-let tasks = [
+const tasks = [
   '散歩',
   '掃除',
   '買い物'
 ];
 
-//関数を定義する
 function showTaskList() {
   tasks.forEach((index, tasks,) => console.log(tasks, index,));
 }
 
-//コンソールに出力
 showTaskList();
 
-//追加入力するタスク一覧を表示する
 const result = prompt(
   '「確認、追加、削除、終了」の4つのいずれかを入力してください');
 
-//タスクが追加されたら表示する
 window.alert = alert(
   '「タスク」が追加されました');
   
-//コンソールに出力
 add();
 
-//タスクに追加したタスク一覧をコンソールに出力
 tasks.push(result);
 showTaskList();
