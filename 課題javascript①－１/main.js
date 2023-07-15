199907a6c3d3d33f19a206672886088cfec6317f
shinnosuke565 <damking420@gmail.com>
2023-07-15 11:03:06 +0900

    修正しました。

--- 課題javascript①－１/main.js
+++ 課題javascript①－１/main.js
@@ -96,37 +96,31 @@ showTaskList();
 //ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 
 //課題③
-// タスクのIDと内容を管理するための配列
 let tasks = [];
 
-// 追加ボタンが押されたときのイベントリスナー
 document.getElementById('addButton').addEventListener('click', () => {
-  const taskName = document.getElementById('addTask').value; // 追加するタスクの名前を取得
-
-  // tasks配列に新たなタスクを追加
+  const taskName = document.getElementById('addTask').value;
   tasks.push({
-    id: tasks.length, // 現在のtasks配列の長さをIDとする（0からスタート）
-    name: taskName, // タスクの名前
-    state: '作業中' // 初期状態は"作業中"
+    id: tasks.length,
+    name: taskName,
+    state: '作業中'
   });
 
-  document.getElementById('addTask').value = ''; // 入力欄を空にする
+  document.getElementById('addTask').value = '';
 
-  displayTasks(); // タスク一覧を表示更新
+  displayTasks();
 });
 
-// タスク一覧を表示する関数
 function displayTasks() {
   const tbody = document.querySelector('#todoTable tbody');
-  tbody.innerHTML = ''; // tbodyの中身を空にする（初期化）
+  tbody.innerHTML = '';
 
   tasks.forEach((task, index) => {
-    const row = tbody.insertRow(-1); // 新しい行を最後に追加
-    row.insertCell(-1).innerText = index; // "ID"列
-    row.insertCell(-1).innerText = task.name; // "コメント"列
-    const stateCell = row.insertCell(-1); // "状態"列
+    const row = tbody.insertRow(-1);
+    row.insertCell(-1).innerText = index;
+    row.insertCell(-1).innerText = task.name;
+    const stateCell = row.insertCell(-1);
 
-    // 状態ボタンの作成
     const stateButton = document.createElement('button');
     stateButton.innerText = task.state;
     stateCell.appendChild(stateButton);
